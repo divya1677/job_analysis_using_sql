fe929e0b606387f9f8807c479351f4ed665f6543
divya1677 <94949861+divya1677@users.noreply.github.com>
2026-02-25 18:03:32 +0530

    Create analysis.sql

---
+++ analysis.sql
@@ -0,0 +1,10 @@
+CREATE TABLE jobs (
+    job_id INT,
+    title VARCHAR(100),
+    location VARCHAR(100),
+    pay_period VARCHAR(20),
+    min_salary INT,
+    max_salary INT,
+    work_type VARCHAR(30),
+    remote_allowed BOOLEAN
+);
